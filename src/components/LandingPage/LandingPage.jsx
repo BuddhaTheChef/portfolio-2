3a201398047b2eb2c0785a03290cdd815b8f28bc
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-16 16:26:18 -0600

    inital setup

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="main-landing-div">
+      <div className="foreground">
+      <h1 className="landing-page-main-title">Aj's Portfolio</h1>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
