d9d6f54b93a738fbab4346073c6dfbcca3cf3737
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-30 16:04:05 -0800

    changed structure for about section

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -32,20 +32,39 @@ class LandingPage extends Component {
        </div>
      </div>
      </div>
+    {/* /// */}
 
-      <div id="about" className="main-about-div">
-      <h1 className="landing-page-main-title">About me</h1>
-      <div className="about-main-div">
-      <div className="about-image-div">
-        Nice quality pic :)
-      </div>
-      <div className="about-content-div">
-        description about yourself
-      </div>
-      </div>
-      </div>
+    <div id="about" className="parent">
+    <h1 className="about-main-title">About</h1>
+  <div className="child child--top">
+    <div className="child__inner">
+    <p className="about-description">
+     Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
+      of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,
+       a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure 
+       Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word
+        in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 
+        1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
+         written in 45 BC. This book is a treatise on the theory of ethics, very popular during the 
+         Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
+          section 1.10.32.
+      </p>
+    </div>
+  </div>
+  <div className="child child--middle">
+    <div className="child__inner">
+      Full Stack Developer 
+    </div>
+  </div>
+  <div className="child child--bottom">
+    <div className="child__inner">
+    Picture :)
+    </div>
+  </div>
+</div>
 
-
+ {/* /// */}
+ 
       <footer className="footer-div">
       <div style={{display:'-webkit-inline-box'}}> 
       <div style={{width: 'fit-content', maxWidth: '333px'}}>
