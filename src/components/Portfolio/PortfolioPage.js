b23742d1d9a7930577014e1c36e228aaa6dc28a9
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-12-07 17:26:38 -0800

    added in on hover events for future projects

--- src/components/Portfolio/PortfolioPage.js
+++ src/components/Portfolio/PortfolioPage.js
@@ -4,34 +4,138 @@ const PortfolioPage = () => (
   <div id="portfolio" className="main-portfolio-div">
     {/* topDiv */}
     <div className="top-portfolio-div">
-      <div className="sub-portfolio-div">Proj 1</div>
+
       <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 1</div>
         <div className="overlay">
-        Proj 2
+        Proj 1 name
         </div>
       </div>
-      <div className="sub-portfolio-div">Proj 3</div>
-      <div className="sub-portfolio-div">Proj 4</div>
-      <div className="sub-portfolio-div">Proj 5</div>
-      <div className="sub-portfolio-div">Proj 6</div>
+
+      <div className="sub-portfolio-div">
+        <div style={{position: "absolute"}}>Proj 2</div>
+        <div className="overlay">
+        Proj 2 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 3</div>
+        <div className="overlay">
+        Proj 3 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 4</div>
+        <div className="overlay">
+        Proj 4 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 5</div>
+        <div className="overlay">
+        Proj 5 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 6</div>
+        <div className="overlay">
+        Proj 6 name
+        </div>
+      </div>
     </div>
+
     {/* midDiv */}
     <div className="top-portfolio-div" style={{ background: "dimgray" }}>
-      <div className="sub-portfolio-div">Proj 7</div>
-      <div className="sub-portfolio-div">Proj 8</div>
-      <div className="sub-portfolio-div">Proj 9</div>
-      <div className="sub-portfolio-div">Proj 10</div>
-      <div className="sub-portfolio-div">Proj 11</div>
-      <div className="sub-portfolio-div">Proj 12</div>
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 7</div>
+        <div className="overlay">
+        Proj 7 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 8</div>
+        <div className="overlay">
+        Proj 8 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 9</div>
+        <div className="overlay">
+        Proj 9 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 10</div>
+        <div className="overlay">
+        Proj 10 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 11</div>
+        <div className="overlay">
+        Proj 11 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 12</div>
+        <div className="overlay">
+        Proj 12 name
+        </div>
+      </div>
     </div>
+
     {/* BotDiv */}
     <div className="top-portfolio-div" style={{ background: "#282c34" }}>
-      <div className="sub-portfolio-div">Proj 13</div>
-      <div className="sub-portfolio-div">Proj 14</div>
-      <div className="sub-portfolio-div">Proj 15</div>
-      <div className="sub-portfolio-div">Proj 16</div>
-      <div className="sub-portfolio-div">Proj 17</div>
-      <div className="sub-portfolio-div">Proj 18</div>
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 13</div>
+        <div className="overlay">
+        Proj 13 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 14</div>
+        <div className="overlay">
+        Proj 14 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 15</div>
+        <div className="overlay">
+        Proj 15 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 16</div>
+        <div className="overlay">
+        Proj 16 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 17</div>
+        <div className="overlay">
+        Proj 17 name
+        </div>
+      </div>
+
+      <div className="sub-portfolio-div">
+      <div style={{position: "absolute"}}>Proj 18</div>
+        <div className="overlay">
+        Proj 18 name
+        </div>
+      </div>
     </div>
   </div>
 );
